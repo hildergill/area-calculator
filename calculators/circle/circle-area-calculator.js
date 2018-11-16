c71a347e4660545d1648676d8ca02410996b02f4
Hilder Gill <hildergill@gmail.com>
2018-11-16 20:01:47 +0800

    Completed circle area calculator

---
+++ calculators/circle/circle-area-calculator.js
@@ -0,0 +1,8 @@
+function calculateCircleArea() {
+    var radius = parseFloat(document.getElementById('radius_input').value);
+    var area = Math.PI * radius * radius;
+
+    var answerBox = document.getElementById('answer_box');
+    answerBox.classList.add('answer_box');
+    answerBox.innerHTML = '<p>' + area + '</p>';
+}
