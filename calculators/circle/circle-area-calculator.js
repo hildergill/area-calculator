f9dc8d30585f57a089d7dbd9be461a5aa1fe9c70
hildergill <hildergill@gmail.com>
2020-04-18 11:41:03 +0800

    Removed all old files

--- calculators/circle/circle-area-calculator.js
+++
@@ -1,8 +0,0 @@
-function calculateCircleArea() {
-    var radius = parseFloat(document.getElementById('radius_input').value);
-    var area = Math.PI * radius * radius;
-
-    var answerBox = document.getElementById('answer_box');
-    answerBox.classList.add('answer_box');
-    answerBox.innerHTML = '<p>&pi; * ('+ radius + ')<sup>2</sup> = ' + area + ' unit<sup>2</sup></p>';
-}
