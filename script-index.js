5cc85a0a563b5f0fe93ed514fa42205d1a758669
hildergill <hildergill@gmail.com>
2020-04-19 21:53:58 +0800

    Backup commit

--- script-index.js
+++ script-index.js
@@ -1,8 +1,22 @@
-var calculatorArray = [new Circle(), new Triangle()];
+var shapeArray = [new Circle(), new Triangle()];
 
 function loadShapes() {
 	var buttonSection = document.getElementById('buttons_section');
-	calculatorArray.forEach((element) => {
-		buttonSection.innerHTML += '<button>' + element.getName() + '</button>';
+	shapeArray.forEach((element, index) => {
+		buttonSection.innerHTML += '<button onclick="loadInput(' + index + ');">' + element.getName() + '</button>';
 	});
+}
+
+function loadInput(shapeIndex) {
+	var inputBox = document.getElementById('input_box');
+	var buttons = document.getElementById('buttons_section').getElementsByTagName('button');
+
+	inputBox.innerHTML = shapeArray[shapeIndex].generateInput();
+
+	for (var i = 0; i < buttons.length; i++) {
+		if (i === shapeIndex) {
+			console.log('Index: ' + shapeIndex);
+			buttons[shapeIndex].classList.add('active');
+		} else buttons[shapeIndex].classList = null;
+	}
 }
