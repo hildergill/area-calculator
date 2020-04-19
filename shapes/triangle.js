4948c3b757be9d38cc8c6ccf033e3f57dd789dbb
hildergill <hildergill@gmail.com>
2020-04-20 06:34:26 +0800

    Update triangle.js

--- shapes/triangle.js
+++ shapes/triangle.js
@@ -10,7 +10,7 @@ class Triangle extends Shape {
 	generateInput() {
 		var styleArray = [
 			'display: grid;',
-			'grid-template-columns: max-content auto;',
+			'grid-template-columns: max-content auto max-content;',
 			'grid-template-rows: auto auto;',
 			'gap: 0.5rem;',
 		];
@@ -24,8 +24,10 @@ class Triangle extends Shape {
 			'<div style="' + styleString + '">',
 			'<p>Width: </p>',
 			'<input type="number" step="0.01" id="width_input">',
+			'<p>unit</p>',
 			'<p>Height: </p>',
 			'<input type="number" step="0.01" id="height_input">',
+			'<p>unit</p>',
 			'</div>',
 		]);
 	}
