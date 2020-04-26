34f3ae9080d6528071df1cea407ed0b30d3309b9
hildergill <hildergill@gmail.com>
2020-04-26 18:11:14 +0800

    Backup commit

--- generators.js
+++ generators.js
@@ -2,6 +2,5 @@ function generateCommonBox(elements, id = null) {
     var returnValue = (id !== null) ? `<div class="common_box" id="${id}">` : '<div class="common_box">';
     elements.map((value) => { returnValue += value + '\n'; });
     returnValue += '</div>';
-
     return returnValue;
 }
