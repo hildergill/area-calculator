09de4c1067c4ab96f5520f1074d1a1886aab0984
hildergill <hildergill@gmail.com>
2020-04-27 09:54:25 +0800

    Added function to change active shape button

--- index.js
+++ index.js
@@ -2,9 +2,19 @@ const shapeArray = [new Circle(), new Triangle()];
 var shapeIndex = 0;
 
 function loadShapeButtons() {
-    var shapeButtons = document.getElementById('shape_buttons_box');
+    var shapeButtonArray = document.getElementById('shape_buttons_box');
 
-    shapeArray.map((value) => {
-        shapeButtons.innerHTML += `<button>\n${value.getIcon()}\n<p>${value.getName()}</p>\n</button>`;
+    shapeArray.map((value, index) => {
+        shapeButtonArray.innerHTML += `<button onclick="loadInput(${index})">\n${value.getIcon()}\n<p>${value.getName()}</p>\n</button>`;
     });
+}
+
+function loadInput(index) {
+    var shapeButtonArray = document.getElementById('shape_buttons_box').getElementsByTagName('button');
+    shapeIndex = index;
+
+    for (var i = 0; i < shapeButtonArray.length; i++) {
+        if (i === index) shapeButtonArray[i].classList.add('active');
+        else shapeButtonArray[i].classList.remove('active');
+    }
 }
