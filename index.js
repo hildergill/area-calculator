06b841254fe1bb5eba6fc2ac841a00c66ae81121
hildergill <hildergill@gmail.com>
2020-04-27 09:57:11 +0800

    AUTO FORMATTED codes

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const shapeArray = [new Circle(), new Triangle()];
 var shapeIndex = 0;
 
 function loadShapeButtons() {
-    var shapeButtonArray = document.getElementById('shape_buttons_box');
+	var shapeButtonArray = document.getElementById('shape_buttons_box');
 
-    shapeArray.map((value, index) => {
-        shapeButtonArray.innerHTML += `<button onclick="loadInput(${index})">\n${value.getIcon()}\n<p>${value.getName()}</p>\n</button>`;
-    });
+	shapeArray.map((value, index) => {
+		shapeButtonArray.innerHTML +=
+			`<button onclick="loadInput(${index})">` +
+			`\n${value.getIcon()}` +
+			`\n<p>${value.getName()}</p>` +
+			`\n</button>`;
+	});
 }
 
 function loadInput(index) {
-    var shapeButtonArray = document.getElementById('shape_buttons_box').getElementsByTagName('button');
-    shapeIndex = index;
+	var shapeButtonArray = document.getElementById('shape_buttons_box').getElementsByTagName('button');
 
-    for (var i = 0; i < shapeButtonArray.length; i++) {
-        if (i === index) shapeButtonArray[i].classList.add('active');
-        else shapeButtonArray[i].classList.remove('active');
-    }
+	shapeIndex = index;
+
+	for (var i = 0; i < shapeButtonArray.length; i++) {
+		if (i === index) shapeButtonArray[i].classList.add('active');
+		else shapeButtonArray[i].classList.remove('active');
+	}
 }
